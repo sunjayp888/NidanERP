f93f0bca0e727b81d72f56a0326b57d30541a2df
vijayraut93 <vijayraut33@gmail.com>
2017-06-05 18:02:57 +0530

    ExpenseHeader CRUD with Bug Fixes

---
+++ source/Nidan/Nidan/Controllers/ExpenseHeaderController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Nidan.Business.Interfaces;
+using Nidan.Entity;
+using Nidan.Entity.Dto;
+using Nidan.Extensions;
+using Nidan.Models;
+
+namespace Nidan.Controllers
+{
+    public class ExpenseHeaderController : BaseController
+    {
+        public ExpenseHeaderController(INidanBusinessService nidanBusinessService) : base(nidanBusinessService)
+        {
+        }
+
+        // GET: ExpenseHeader
+        public ActionResult Index()
+        {
+            return View(new BaseViewModel());
+        }
+
+        // GET: ExpenseHeader/Create
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create()
+        {
+            var viewModel = new ExpenseHeaderViewModel
+            {
+                ExpenseHeader = new ExpenseHeader(),
+            };
+
+            return View(viewModel);
+        }
+
+        // POST: ExpenseHeader/Create
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ExpenseHeaderViewModel expenseHeaderViewModel)
+        {
+            var organisationId = UserOrganisationId;
+            if (ModelState.IsValid)
+            {
+                expenseHeaderViewModel.ExpenseHeader.OrganisationId = organisationId;
+                expenseHeaderViewModel.ExpenseHeader = NidanBusinessService.CreateExpenseHeader(organisationId, expenseHeaderViewModel.ExpenseHeader);
+                return RedirectToAction("Index");
+            }
+            return View(expenseHeaderViewModel);
+        }
+
+        // GET: ExpenseHeader/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var organisationId = UserOrganisationId;
+            var expenseHeader = NidanBusinessService.RetrieveExpenseHeader(organisationId, id.Value, e => true);
+
+            if (expenseHeader == null)
+            {
+                return HttpNotFound();
+            }
+            var viewModel = new ExpenseHeaderViewModel
+            {
+                ExpenseHeader = expenseHeader
+            };
+            return View(viewModel);
+        }
+
+        // POST: ExpenseHeader/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ExpenseHeaderViewModel expenseHeaderViewModel)
+        {
+            var organisationId = UserOrganisationId;
+            if (ModelState.IsValid)
+            {
+                expenseHeaderViewModel.ExpenseHeader.OrganisationId = organisationId;
+                expenseHeaderViewModel.ExpenseHeader = NidanBusinessService.UpdateExpenseHeader(organisationId, expenseHeaderViewModel.ExpenseHeader);
+                return RedirectToAction("Index");
+            }
+            var viewModel = new ExpenseHeaderViewModel
+            {
+                ExpenseHeader = expenseHeaderViewModel.ExpenseHeader
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult List(Paging paging, List<OrderBy> orderBy)
+        {
+            return this.JsonNet(NidanBusinessService.RetrieveExpenseHeaders(UserOrganisationId, p => true, orderBy, paging));
+        }
+    }
+}
---
+++ source/Nidan/Nidan/Scripts/Angular/Controllers/ExpenseHeaderController.js
@@ -0,0 +1,92 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('Nidan')
+        .controller('ExpenseHeaderController', ExpenseHeaderController);
+
+    ExpenseHeaderController.$inject = ['$window', 'ExpenseHeaderService', 'Paging', 'OrderService', 'OrderBy', 'Order', '$uibModal'];
+
+    function ExpenseHeaderController($window, ExpenseHeaderService, Paging, OrderService, OrderBy, Order, $uibModal, $modalInstance) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.expenseHeaders = [];
+        vm.paging = new Paging;
+        vm.pageChanged = pageChanged;
+        vm.orderBy = new OrderBy;
+        vm.order = order;
+        vm.orderClass = orderClass;
+        vm.editExpenseHeader = editExpenseHeader;
+        vm.canDeleteExpenseHeader = canDeleteExpenseHeader;
+        vm.deleteExpenseHeader = deleteExpenseHeader;
+        vm.searchExpenseHeader = searchExpenseHeader;
+        vm.viewExpenseHeader = viewExpenseHeader;
+        vm.searchKeyword = "";
+        vm.searchMessage = "";
+        initialise();
+
+        function initialise() {
+            vm.orderBy.property = "ExpenseHeaderId";
+            vm.orderBy.direction = "Ascending";
+            vm.orderBy.class = "asc";
+            order("ExpenseHeaderId");
+        }
+
+        function retrieveExpenseHeaders() {
+            return ExpenseHeaderService.retrieveExpenseHeaders(vm.paging, vm.orderBy)
+                .then(function (response) {
+                    vm.expenseHeaders = response.data.Items;
+                    vm.paging.totalPages = response.data.TotalPages;
+                    vm.paging.totalResults = response.data.TotalResults;
+                    return vm.expenseHeaders;
+                });
+        }
+
+        function searchExpenseHeader(searchKeyword) {
+            vm.searchKeyword = searchKeyword;
+            return ExpenseHeaderService.searchExpenseHeader(vm.searchKeyword, vm.paging, vm.orderBy)
+                .then(function (response) {
+                    vm.expenseHeaders = response.data.Items;
+                    vm.paging.totalPages = response.data.TotalPages;
+                    vm.paging.totalResults = response.data.TotalResults;
+                    vm.searchMessage = vm.expenseHeaders.length === 0 ? "No Records Found" : "";
+                    return vm.expenseHeaders;
+                });
+        }
+
+        function pageChanged() {
+            return retrieveExpenseHeaders();
+        }
+
+        function order(property) {
+            vm.orderBy = OrderService.order(vm.orderBy, property);
+            return retrieveExpenseHeaders();
+        }
+
+        function orderClass(property) {
+            return OrderService.orderClass(vm.orderBy, property);
+        }
+
+        function editExpenseHeader(id) {
+            $window.location.href = "/ExpenseHeader/Edit/" + id;
+        }
+
+        function canDeleteExpenseHeader(id) {
+            vm.loadingActions = true;
+            vm.CanDeleteExpenseHeader = false;
+            $('.dropdown-menu').slideUp('fast');
+            $('.' + id).toggle();
+            ExpenseHeaderService.canDeleteExpenseHeader(id).then(function (response) { vm.CanDeleteExpenseHeader = response.data, vm.loadingActions = false });
+        }
+
+        function deleteExpenseHeader(id) {
+            return ExpenseHeaderService.deleteExpenseHeader(id).then(function () { initialise(); });
+        };
+
+        function viewExpenseHeader(expenseHeaderId) {
+            $window.location.href = "/ExpenseHeader/Edit/" + expenseHeaderId;
+        }
+
+    }
+
+})();
